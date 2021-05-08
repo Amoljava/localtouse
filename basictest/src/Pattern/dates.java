5124a59773fcec169d9933f49b83c1419d645a19
asd@123 <asd@gmail.com>
2021-05-08 11:31:32 +0530

    date modify

--- basictest/src/Pattern/dates.java
+++ basictest/src/Pattern/dates.java
@@ -1,5 +1,14 @@
 package Pattern;
 
-public class dates {
+import java.util.Date;
 
+public class dates {
+	
+	public static void main(String[] args) {
+		
+		Date d1=new Date();
+		System.out.println(d1.getDate());
+	}
 }
+
+
