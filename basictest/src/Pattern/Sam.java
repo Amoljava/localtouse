68a487ad9bcb5256c77ab01350998e25aebb861e
eshwar4518 <esh4518@gmail.com>
2021-05-17 12:48:13 +0530

    added addition method in Sam.java

--- basictest/src/Pattern/Sam.java
+++ basictest/src/Pattern/Sam.java
@@ -7,16 +7,11 @@ import javax.jws.Oneway;
 
 public class Sam {
 	public static void main(String[] args) {
-		HashSet<Ex> h1= new HashSet<Ex>();
-		Ex x1= new Ex(5 , 6);
-		Ex x2= new Ex(7 , 4);
-		System.out.println("new : "+x1.hashCode());
-		System.out.println(x2.hashCode());
-		h1.add(new Ex(5 , 6));
-		h1.add(new Ex(1 , 2));
-		h1.add(new Ex(7 , 4));
-		System.out.println(h1);
-		
+		System.out.println(add(10,15));
+			
+		}
+	public static int add(int num1, int num2) {
+		return num1+num2;
 	}
 
 }
