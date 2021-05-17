406e9c47330bf503c5c6933d6ab7a0f4ece0122d
eshwar4518 <esh4518@gmail.com>
2021-05-17 12:32:36 +0530

    Hi Amol...Committed changes in dates.java

--- basictest/src/Pattern/dates.java
+++ basictest/src/Pattern/dates.java
@@ -6,8 +6,7 @@ public class dates {
 	
 	public static void main(String[] args) {
 		
-		Date d1=new Date();
-		System.out.println(d1.getDate());
+		System.out.println("Committed by Eshwar...!");
 	}
 }
 
